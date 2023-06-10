5b53cb7ea71afb6a0bed00b24ab99b49f1cd444a
Melek <99866191+MELEKGEZER@users.noreply.github.com>
2023-06-11 01:42:46 +0300

    Add files via upload

---
+++ harmonik.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class harmonik {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Bir sayı girin: ");
+        int sayi = scanner.nextInt();
+        
+        double harmonikSerisi = 0;
+        
+        for (int i = 1; i <= sayi; i++) {
+            harmonikSerisi += 1.0 / i;
+        }
+        
+        System.out.println("Harmonik Serisi: " + harmonikSerisi);
+    }
+}
+
