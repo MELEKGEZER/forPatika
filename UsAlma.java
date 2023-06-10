67fdd2cf687d47cf87a0d98d2b992bb3d2d84bb3
Melek <99866191+MELEKGEZER@users.noreply.github.com>
2023-06-11 01:36:05 +0300

    Add files via upload

---
+++ UsAlma.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class UsAlma {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Taban değerini girin: ");
+        int taban = scanner.nextInt();
+        
+        System.out.print("Üs değerini girin: ");
+        int us = scanner.nextInt();
+        
+        int sonuc = 1;
+        
+        for (int i = 1; i <= us; i++) {
+            sonuc *= taban;
+        }
+        
+        System.out.println(taban + "^" + us + " = " + sonuc);
+    }
+}
